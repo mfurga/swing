ec7c2bf72188a0582454e328fca0c1fb157244a9
Mateusz Furga <matfurga@gmail.com>
2020-07-04 10:56:44 +0200

    Add swg_core.h

---
+++ src/swg_core.h
@@ -0,0 +1,12 @@
+#ifndef _SWG_CORE_H_INCLUDED_
+#define _SWG_CORE_H_INCLUDED_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdarg.h>
+
+typedef uintptr_t     swg_uint_t;
+
+#endif  // _SWG_CORE_H_INCLUDED_
